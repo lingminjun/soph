b53a3ef2592936a81e474aae64060055360d46ae
Ling Minjun <lingminjun@lingminjun-MacBook-Pro.local>
2018-04-27 12:49:01 +0800

    测试代码提交

--- src/main/java/ssn/lmj/Application.java
+++ src/main/java/ssn/lmj/Application.java
@@ -1,5 +1,6 @@
 package ssn.lmj;
 
+import com.alibaba.fastjson.JSON;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,7 +17,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //@MapperScan("ssn.lmj.soph.db")
 public class Application {
 
+//    static class TObj {
+//        public String val1;
+//        public int val2;
+//        public String val3;
+//        public int val4;
+//        public float val5;
+//        public long val6;
+//        public boolean val7;
+//    }
+
     public static void main(String[] args) {
+
+//        String json = "{\"val1\":123,\"val2\":123.1,\"val3\":123,\"val4\":\"453\"," +
+//                "\"val5\":123,\"val6\":\"44\",\"val7\":\"0\"}";
+//        TObj obj = JSON.parseObject(json,TObj.class);
+//
+//        System.out.println(">>>>>\n" + JSON.toJSONString(obj));
+
         // 程序启动入口
         // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
         SpringApplication.run(Application.class,args);
